c12e01ba97f08754cc57369ee688eb172f20ce7d
Dinesh Chandran <dineshchandran311@gmail.com>
2021-12-08 14:28:38 +0530

    Express Mongoose with REST API part 1

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+    console.log('Connected correctly to the server');
+})
+.catch((err) => console.log("Error occured"));
+
 var app = express();
 
 // view engine setup
