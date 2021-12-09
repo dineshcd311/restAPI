2044af177aa22092fa20df81f1da790b5f68a04a
Dinesh Chandran <dineshchandran311@gmail.com>
2021-12-09 10:17:46 +0530

    After part 1 auth

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Promotions = require('../models/promotion');
+const Promotions = require('../models/promotions');
 
 const promoRouter = express.Router();
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+const User = require('./models/user');
+
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', function(req, res, next){
+    User.findOne({username: req.body.username})
+    .then( (user) => {
+        if(user!=null)
+        {
+            var err = new Error('User ' + req.body.username + ' already exist');
+            err.status = 403;
+            next(err);    
+        }
+        else{
+            return User.create({
+                username: req.body.username,
+                password: req.body.password
+            })
+        }
+    })
+    .then( (user) =>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json({status: 'Registeration Successful!', user: user});
+    }, (err) => next(err))
+    .catch( (err) => next(err));
 });
 
 module.exports = router;
